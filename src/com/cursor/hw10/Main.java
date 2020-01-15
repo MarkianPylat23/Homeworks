3c100e0221c193d0eb634c9a52c69d10db3c992b
MarkianPylat23 <44113406+MarkianPylat23@users.noreply.github.com>
2020-01-15 20:20:50 +0200

    Add files via upload

---
+++ src/com/cursor/hw10/Main.java
@@ -0,0 +1,9 @@
+package com.cursor.hw10;
+
+import com.cursor.hw10.app.runner.AppRunner;
+
+public class Main {
+    public static void main(String[] args) {
+        new AppRunner().runApp();
+    }
+}
