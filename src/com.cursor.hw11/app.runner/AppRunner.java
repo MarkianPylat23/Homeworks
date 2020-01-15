f7c648609824241d1b7fff24696d353ce44b3592
MarkianPylat23 <marek.pt23@gmail.com>
2020-01-15 12:41:18 +0200

    add hw11

--- src/com.cursor.hw11/app.runner/AppRunner.java
+++ src/com.cursor.hw11/app.runner/AppRunner.java
@@ -4,7 +4,7 @@ public class AppRunner {
     public void runApp() {
         int[] array = new int[Integer.MAX_VALUE];
 
-        for (int i = 0; i < array.length; ++i)
-            array[i] = i + 1;
+        for (int index = 0; index < array.length; index++)
+            array[index] = index + 1;
     }
 }
