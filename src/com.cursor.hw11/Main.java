a2746773750cc8a4510ab68f6c0ce11c33fde621
MarkianPylat23 <44113406+MarkianPylat23@users.noreply.github.com>
2020-05-07 13:14:36 +0300

    Delete Main.java

--- src/com.cursor.hw11/Main.java
+++
@@ -1,9 +0,0 @@
-package com.cursor.hw11;
-
-import com.cursor.hw11.app.runner.AppRunner;
-
-public class Main {
-    public static void main(String[] args) {
-        new AppRunner().runApp();
-    }
-}
