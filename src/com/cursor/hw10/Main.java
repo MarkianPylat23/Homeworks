9b5b732adc2b9cff056b3f70ae2499aa09535c06
MarkianPylat23 <marek.pt23@gmail.com>
2020-05-07 10:24:05 +0300

    Rewrite mistakes

--- src/com/cursor/hw10/Main.java
+++
@@ -1,9 +0,0 @@
-package com.cursor.hw10;
-
-import com.cursor.hw10.app.runner.AppRunner;
-
-public class Main {
-    public static void main(String[] args) {
-        //new AppRunner().runApp();
-    }
-}
